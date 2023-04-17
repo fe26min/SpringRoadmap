bc00f2f113711faeaf7c0d0741a63e8320363768
fe26min <118718073+fe26min@users.noreply.github.com>
2023-04-17 11:30:13 +0900

    [스프링 입문] AOP : AOP 적용
    [스프링 입문] AOP : AOP 적용

--- 스프링 입문/hello-spring/src/main/java/helllo/hellospring/service/MemberService.java
+++ 스프링 입문/hello-spring/src/main/java/helllo/hellospring/service/MemberService.java
@@ -19,31 +19,17 @@ public class MemberService {
     // 회원 가입
     public Long join(Member member) {
 
-        long start = System.currentTimeMillis();
-        try {
-            validateDuplicateMember(member); //중복 회원 검증
-            memberRepository.save(member);
-            return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join = " + timeMs + "ms");
-        }
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
 
-        long start = System.currentTimeMillis();
-        try {
-            memberRepository.findByName(member.getName())
-                    .ifPresent(m -> {
-                        throw new IllegalStateException("이미 존재하는 회원입니다.");
-                    });
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("join = " + timeMs + "ms");
-        }
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
     }
 
     // 전체 회원 조회
