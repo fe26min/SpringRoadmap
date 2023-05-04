423ec6eeaca597e55b367fd537939fe91255243f
fe26min <118718073+fe26min@users.noreply.github.com>
2023-05-04 09:44:15 +0900

    [스프링 핵심 원리 - 기본편] 섹션 7 의존관계 자동 주입 : 옵션 처리
    [스프링 핵심 원리 - 기본편] 섹션 7 의존관계 자동 주입 : 옵션 처리

--- 스프링 핵심 원리/core/src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ 스프링 핵심 원리/core/src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -29,7 +29,7 @@ public class ApplicationContextExtendsFindTest {
     @Test
     @DisplayName("부모 타입으로 조회시, 자식이 둘 이상 있으면, 빈 이르을 지정하면 된다")
     void findBeanByParentTypeBeanName() {
-        DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPloicy", DiscountPolicy.class);
+        DiscountPolicy rateDiscountPolicy = ac.getBean("rateDiscountPolicy", DiscountPolicy.class);
         assertThat(rateDiscountPolicy).isInstanceOf(RateDiscountPolicy.class);
     }
 
@@ -63,7 +63,7 @@ public class ApplicationContextExtendsFindTest {
     @Configuration
     static class TestConfig {
         @Bean
-        public DiscountPolicy reteDiscountPolicy() {
+        public DiscountPolicy rateDiscountPolicy() {
             return new RateDiscountPolicy();
         }
 
