aa6eb70b5de343b28fd2af009fc735346dbf6ed7
fe26min <118718073+fe26min@users.noreply.github.com>
2023-06-15 11:17:14 +0900

    [실전! 스프링 부트와 JPA 활용1] 섹션 7.웹 계층 개발 : 상품 수정
    [실전! 스프링 부트와 JPA 활용1] 섹션 7.웹 계층 개발 : 상품 수정

--- 실전! 스프링 부트와 JPA 활용1/jpashop/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ 실전! 스프링 부트와 JPA 활용1/jpashop/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -3,10 +3,11 @@ package jpabook.jpashop.controller;
 import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.Banner;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -41,5 +42,34 @@ public class ItemController {
         return "items/itemList";
     }
 
+    @GetMapping("/items/{itemID}/edit")
+    public String updateItemForm(@PathVariable("itemID") Long itemId, Model model) {
+        Book item = (Book) itemService.findOne(itemId);
+
+        BookForm form = new BookForm();
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+    }
+
+    @PostMapping("/items/{itemID}/edit")
+    public String updateItem(@ModelAttribute("form") BookForm form) {
+        Book book = new Book();
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
+    }
 
 }
