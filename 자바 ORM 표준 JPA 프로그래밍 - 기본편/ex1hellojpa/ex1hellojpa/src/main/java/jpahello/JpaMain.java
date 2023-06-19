8265a470fcd066d522d1c26780f489be0c942b88
fe26min <118718073+fe26min@users.noreply.github.com>
2023-06-19 23:06:36 +0900

    [자바 ORM 표준 JPA 프로그래밍 - 기본편] 섹션 5. 연관관계 매핑기초 :단반향 연관관계
    [자바 ORM 표준 JPA 프로그래밍 - 기본편] 섹션 5. 연관관계 매핑기초 :단반향 연관관계

--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1hellojpa/ex1hellojpa/src/main/java/jpahello/JpaMain.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1hellojpa/ex1hellojpa/src/main/java/jpahello/JpaMain.java
@@ -15,15 +15,27 @@
             EntityTransaction tx = em.getTransaction();
             tx.begin();
             try {
+
+                Team team = new Team();
+                team.setName("TeamA");
+                em.persist(team);
+
                 Member member = new Member();
-                member.setUsername("C");
-
-                System.out.println("================");
-
+                member.setUsername("member1");
+                member.setTeam(team);
                 em.persist(member);
 
-                System.out.println("member.id = " + member.getId());
-                System.out.println("================");
+                em.flush();
+                em.clear();
+
+                Member findMember = em.find(Member.class, member.getId());
+
+                Team findTeam = findMember.getTeam();
+                System.out.println("findTeam = " + findTeam.getName());
+
+                //
+                Team newTeam = em.find(Team.class, 100L);
+                findMember.setTeam(newTeam);
 
                 tx.commit();
             } catch (Exception e) {
--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1hellojpa/ex1hellojpa/src/main/java/jpahello/Member.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1hellojpa/ex1hellojpa/src/main/java/jpahello/Member.java
@@ -5,19 +5,19 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
 @Entity
-@SequenceGenerator(
-        name = "MEMBER_SEQ_GENERATOR",
-        sequenceName = "MEBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
-        initialValue = 1, allocationSize = 50)
 public class Member {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
-    @Column(name = "name", nullable = false)
+    @Column(name = "USERNAME")
     private String username;
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
 
-    public Member() {
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
     public Long getId() {
         return id;
@@ -33,5 +33,13 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
