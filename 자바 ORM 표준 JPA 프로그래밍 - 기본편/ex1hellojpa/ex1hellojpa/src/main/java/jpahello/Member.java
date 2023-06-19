dbe8d7e5d941089d4581eef289fdc6eab3f5a4e3
fe26min <118718073+fe26min@users.noreply.github.com>
2023-06-19 21:52:34 +0900

    [자바 ORM 표준 JPA 프로그래밍 - 기본편] 섹션 4. 엔티티 매핑 : 기본 키 매핑
    [자바 ORM 표준 JPA 프로그래밍 - 기본편] 섹션 4. 엔티티 매핑 : 기본 키 매핑

--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1hellojpa/ex1hellojpa/src/main/java/jpahello/Member.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1hellojpa/ex1hellojpa/src/main/java/jpahello/Member.java
@@ -5,23 +5,16 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
 @Entity
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1, allocationSize = 50)
 public class Member {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
-    @Column(name = "name")
+    @Column(name = "name", nullable = false)
     private String username;
-    private Integer age;
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    private LocalDate testLocalDate;
-    private LocalDateTime testLocalDateTime;
-    @Lob
-    private String description;
 
     public Member() {
     }
@@ -40,45 +33,5 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 }
