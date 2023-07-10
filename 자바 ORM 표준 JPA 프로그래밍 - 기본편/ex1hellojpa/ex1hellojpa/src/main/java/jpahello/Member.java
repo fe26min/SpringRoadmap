c0a44cde123afddef1e9d6f4578a81b6e004a999
fe26min <118718073+fe26min@users.noreply.github.com>
2023-07-10 18:38:29 +0900

    [자바 ORM 표준 JPA 프로그래밍 - 기본현] 섹션 6.다양한 연관관계 매핑 : 다대다 [N:M]
    [자바 ORM 표준 JPA 프로그래밍 - 기본현] 섹션 6.다양한 연관관계 매핑 : 다대다 [N:M]

--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1hellojpa/ex1hellojpa/src/main/java/jpahello/Member.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1hellojpa/ex1hellojpa/src/main/java/jpahello/Member.java
@@ -3,7 +3,10 @@ package jpahello;
 import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
 @Entity
 public class Member {
     @Id
@@ -20,6 +23,9 @@ public class Member {
     @OneToOne
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
+
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> memberProducts;
 
     public Long getId() {
         return id;
