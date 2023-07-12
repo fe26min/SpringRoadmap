a5223ed3ae650ea902768dd6f573be03fbc67984
fe26min <118718073+fe26min@users.noreply.github.com>
2023-07-12 10:49:50 +0900

    [자바 ORM 표준 JPA 프로그래밍 - 기본편] 섹션 8. 프록시와 연관관계 관리 : 프록시
    [자바 ORM 표준 JPA 프로그래밍 - 기본편] 섹션 8. 프록시와 연관관계 관리 : 프록시

--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1hellojpa/ex1hellojpa/src/main/java/jpahello/JpaMain.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1hellojpa/ex1hellojpa/src/main/java/jpahello/JpaMain.java
@@ -1,9 +1,7 @@
     package jpahello;
 
-    import jakarta.persistence.EntityManager;
-    import jakarta.persistence.EntityManagerFactory;
-    import jakarta.persistence.EntityTransaction;
-    import jakarta.persistence.Persistence;
+    import jakarta.persistence.*;
+    import org.hibernate.Hibernate;
 
     import java.time.LocalDateTime;
     import java.util.List;
@@ -16,23 +14,42 @@
             EntityTransaction tx = em.getTransaction();
             tx.begin();
             try {
+                Member member1 = new Member();
+                member1.setUsername("member1");
+                em.persist(member1);
 
-               Member member = new Member();
-               member.setUsername("user1");
-               member.setCreateBy("kim");
-               member.setCreatedDate(LocalDateTime.now());
+                Member member2 = new Member();
+                member2.setUsername("member2");
+                em.persist(member2);
 
-               em.persist(member);
+                em.flush();
+                em.clear();
 
-               em.flush();
-               em.clear();
+                Member refMember = em.getReference(Member.class, member1.getId());
+                System.out.println("refMember = " + refMember.getClass());
+                Hibernate.initialize(refMember);    //강제 초기화
 
-               tx.commit();
+
+                tx.commit();
             } catch (Exception e) {
+                e.printStackTrace();
                 tx.rollback();
             } finally {
                 em.close();
             }
             emf.close();
+        }
+
+        private static void printMember(Member member) {
+            String username = member.getUsername();
+            System.out.println("username = " + username);
+        }
+
+        private static void printMemberAndTeam(Member member) {
+            String username = member.getUsername();
+            System.out.println("username = " + username);
+
+            Team team = member.getTeam();
+            System.out.println("team = " + team.getName());
         }
     }
--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1hellojpa/ex1hellojpa/src/main/java/jpahello/Member.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1hellojpa/ex1hellojpa/src/main/java/jpahello/Member.java
@@ -20,13 +20,6 @@ public class Member extends BaseEntity{
     @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
     private Team team;
 
-    @OneToOne
-    @JoinColumn(name = "LOCKER_ID")
-    private Locker locker;
-
-    @OneToMany(mappedBy = "member")
-    private List<MemberProduct> memberProducts;
-
     public Long getId() {
         return id;
     }
@@ -41,6 +34,14 @@ public class Member extends BaseEntity{
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 
 }
