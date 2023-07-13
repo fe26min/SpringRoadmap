d1e83e6b81c66197606ffe4430bf16e59951d448
fe26min <118718073+fe26min@users.noreply.github.com>
2023-07-13 10:25:48 +0900

    [자바 ORM 표준 JPA 프로그래밍 - 기본편] 섹션 8. 프록시와 연관관계 정리 : 즉시 로딩과 지연 로딩
    [자바 ORM 표준 JPA 프로그래밍 - 기본편] 섹션 8. 프록시와 연관관계 정리 : 즉시 로딩과 지연 로딩

--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1hellojpa/ex1hellojpa/src/main/java/jpahello/JpaMain.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1hellojpa/ex1hellojpa/src/main/java/jpahello/JpaMain.java
@@ -14,26 +14,30 @@
             EntityTransaction tx = em.getTransaction();
             tx.begin();
             try {
+
+                Team team = new Team();
+                team.setName("teamA");
+                System.out.println(team.getName());
+                em.persist(team);
+
                 Member member1 = new Member();
                 member1.setUsername("member1");
+                member1.setTeam(team);
                 em.persist(member1);
 
-                Member member2 = new Member();
-                member2.setUsername("member2");
-                em.persist(member2);
 
                 em.flush();
                 em.clear();
 
-                Member refMember = em.getReference(Member.class, member1.getId());
-                System.out.println("refMember = " + refMember.getClass());
-                Hibernate.initialize(refMember);    //강제 초기화
+//                Member m = em.find(Member.class, member1.getId());
+
+                em.createQuery("select m from Member m join fetch  m.team", Member.class).getResultList();
 
 
                 tx.commit();
             } catch (Exception e) {
-                e.printStackTrace();
                 tx.rollback();
+                e.printStackTrace();
             } finally {
                 em.close();
             }
